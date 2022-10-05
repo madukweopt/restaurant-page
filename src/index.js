68defd60f3960e929b732113a7b32b9c1b91c812
pascal <madukwepascal@gmail.com>
2022-10-05 20:12:30 +0100

    Add event listerners to home and menu buttons

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import loadHome from './home.js'
 import loadMenu from './menu.js'
 
 loadSite();
+loadHome()
+
+function addMenuEvent() {
+    const menuBtn = document.querySelector('.menu-btn');
+    menuBtn.addEventListener('click', loadMenu);
+}
+addMenuEvent()
+
+function addHomeEvent() {
+    const homeBtn = document.querySelector('.home-btn');
+    homeBtn.addEventListener('click', loadHome);
+}
+
+addHomeEvent()
 
 
 
